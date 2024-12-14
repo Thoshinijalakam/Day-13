7c7faff8b93ce4feaaba7ada7553bb03178242ea
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-14 11:37:15 +0530

    Add files via upload

---
+++ Multilevelinheritance.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class a{
+    public void p1()
+    {
+        System.out.println("Hello Students");
+    }
+}
+class b extends a{
+    public void r(){
+        System.out.println("Hello Teachers");
+    }
+}
+class c extends b{
+    public void r1(){
+        System.out.println("Hello professors");
+    }
+}
+public class Multilevelinheritance{
+    public static void main(String args[]){
+        c f=new c();
+        f.p1();
+        f.r();
+        f.r1();
+    }
+}
+/* Hello Students
+Hello Teachers
+Hello professors */
