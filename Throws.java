62e6c514f194b30bc73d8a2e313516b9720e61c3
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-14 15:43:05 +0530

    Add files via upload

---
+++ Throws.java
@@ -0,0 +1,10 @@
+public class Throws {
+    public static void main(String args[]){
+        int a=10;
+        int b=0;
+        System.out.println(a/b);
+    }
+    
+}
+/* Exception in thread "main" java.lang.ArithmeticException: / by zero
+        at Throws.main(Throws.java:5) */
